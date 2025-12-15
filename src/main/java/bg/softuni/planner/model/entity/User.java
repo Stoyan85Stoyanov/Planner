7699561694f45ead6044e1c71003183a2cf2f2d3
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-12-15 17:47:03 +0200

    Planner

---
+++ src/main/java/bg/softuni/planner/model/entity/User.java
@@ -0,0 +1,28 @@
+package bg.softuni.planner.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User extends BaseEntity{
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(fetch = FetchType.EAGER , mappedBy = "user")
+    private Set<Task> assignedTasks;
+}
