ada8fc6e44104618ba1380e2cdd91d70674687ef
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-12-15 17:47:12 +0200

    Planner

---
+++ src/main/java/bg/softuni/planner/dto/UserLoginDto.java
@@ -0,0 +1,21 @@
+package bg.softuni.planner.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDto {
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String password;
+}
