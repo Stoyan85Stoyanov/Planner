19e6ebd49bdd3c2a4f7219d8bf55d59c6d629ac0
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-12-16 17:17:07 +0200

    Planner

---
+++ src/main/java/bg/softuni/planner/service/UserService.java
@@ -0,0 +1,60 @@
+package bg.softuni.planner.service;
+
+import bg.softuni.planner.config.UserSession;
+import bg.softuni.planner.dto.UserLoginDto;
+import bg.softuni.planner.dto.UserRegisterDto;
+import bg.softuni.planner.model.entity.User;
+import bg.softuni.planner.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserSession userSession;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, UserSession userSession) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userSession = userSession;
+    }
+
+    public boolean register(UserRegisterDto data) {
+        Optional<User> existingUser = userRepository
+                .findByUsernameOrEmail(data.getUsername(), data.getEmail());
+
+        if (existingUser.isPresent()) {
+            return false;
+        }
+
+        User user = new User();
+        user.setUsername(data.getUsername());
+        user.setEmail(data.getEmail());
+        user.setPassword(passwordEncoder.encode(data.getPassword()));
+
+        this.userRepository.save(user);
+
+        return true;
+    }
+
+    public boolean login (UserLoginDto data){
+        Optional<User> byUsername = userRepository.findUserByUsername(data.getUsername());
+
+        if (byUsername.isEmpty()) {
+            return false;
+        }
+
+        boolean passMatch = passwordEncoder.matches(data.getPassword(), byUsername.get().getPassword());
+
+        if (!passMatch) {
+            return false;
+        }
+        userSession.login(byUsername.get().getId(), byUsername.get().getUsername());
+
+        return true;
+    }
+}
